7119f561e4f619bea0148078d3ee65e7e754c5c8
Artem18 <urart88@mail.ru>
2022-06-19 14:44:07 +0400

    Диплом - основы JS ч.1 на проверку

---
+++ public/loginPage.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const userFormOne = new UserForm();
+userFormOne.loginFormCallback = (data) => {
+    ApiConnector.login(data, (response) => {
+        if(ApiConnector.login) {
+            location.reload();
+        } else {
+            userFormOne.setLoginErrorMessage(message);
+        }
+    })
+};
+
+userFormOne.registerFormCallback = (data) => {
+    ApiConnector.register(data, (response) => {
+        if(ApiConnector.login) {
+            location.reload();
+        } else {
+            userFormOne.setLoginErrorMessage(message);
+        }
+    })
+};
